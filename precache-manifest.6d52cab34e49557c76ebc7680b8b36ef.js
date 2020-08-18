788d101880edb6d8f895922f71890f0b7554bc1d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-18 06:19:21 +0000

    Deploy sitcon-tw/CCIP-HDP to github.com/sitcon-tw/CCIP-HDP.git:gh-pages

---
+++ precache-manifest.6d52cab34e49557c76ebc7680b8b36ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee48d84faa18477fb28",
+    "url": "/css/app.3f02ad13.css"
+  },
+  {
+    "revision": "c27013b4fd50412baf2f",
+    "url": "/css/chunk-vendors.5e44303a.css"
+  },
+  {
+    "revision": "cd8681efac21006177ad5c804796eb34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee48d84faa18477fb28",
+    "url": "/js/app.f21b40a9.js"
+  },
+  {
+    "revision": "c27013b4fd50412baf2f",
+    "url": "/js/chunk-vendors.db9426a7.js"
+  },
+  {
+    "revision": "9894a2de7c132bb28bda",
+    "url": "/js/declaration.53d5c413.js"
+  },
+  {
+    "revision": "99ce8641c041efb2183c",
+    "url": "/js/fill.a31566a3.js"
+  },
+  {
+    "revision": "d953b5dffd0a087c2687",
+    "url": "/js/status.0cfb137c.js"
+  },
+  {
+    "revision": "28bf2ab99af2d0770d51a57d31b2f113",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
